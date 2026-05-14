cd7868dbc6c0e7b5094ea332480a1cc01c1415d6
Amir Khedri <khedria95@.com>
2026-05-14 18:16:47 +0330

    feat: add BFS solver for Sokoban

--- search.py
+++ search.py
@@ -1,30 +1,27 @@
+from collections import deque
+import heapq
+from env.sokoban import PUSH_COST, MOVE_COST
+
+# -------------------------------------------------------------------
+# 1. BFS – Breadth‑First Search (shortest action sequence)
+# -------------------------------------------------------------------
 def bfs_solve(game):
     start_state = game.get_initial_state()
+    if game.is_goal(start_state):
+        return []
 
-    # Your code goes here
-    # this function should return actions
+    frontier = deque()
+    frontier.append((start_state, []))
+    visited = {start_state}
 
+    while frontier:
+        state, actions = frontier.popleft()
+        for action, next_state, _ in game.get_successors(state):
+            if next_state in visited:
+                continue
+            if game.is_goal(next_state):
+                return actions + [action]
+            visited.add(next_state)
+            frontier.append((next_state, actions + [action]))
+    return None
 
-def ids_solve(game):
-    def dls_solve(game, max_depth):
-        start_state = game.get_initial_state()
-
-    # Implement the algorithm here
-    # this function should return actions
-
-
-def ucs_solve(game):
-    start_state = game.get_initial_state()
-
-    # Your code goes here
-    # this function should return actions
-
-
-def astar_solve(game):
-    def heuristic():
-        pass
-
-    start_state = game.get_initial_state()
-
-    # Your code goes here
-    # this function should return actions
